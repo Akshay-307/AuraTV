3e85744ad7e6712b908c44589ee47da226a0915a
Akshay <akshay.aura@gmail.com>
2026-06-06 15:04:36 +0530

    Allow sandbox top-navigation to fix detection error, block popups/redirects with beforeunload, add Aura logo image, and replace mock ad with Start.io ad

--- app.js
+++ app.js
@@ -458,7 +458,7 @@ function playMedia(id, type, title, season = 1, episode = 1) {
         }
         
         const iframe = document.getElementById("player-iframe");
-        iframe.removeAttribute("sandbox");
+        iframe.setAttribute("sandbox", "allow-scripts allow-same-origin allow-forms allow-presentation allow-top-navigation");
         iframe.src = embedUrl;
     };
     
